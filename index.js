4f0fa149f1ee34a3d7fdf49f46ccf82e36a1ad00
Alex Myers <alex.myers919@gmail.com>
2022-03-22 23:38:13 -0400

    pre-intermediate complete

---
+++ index.js
@@ -0,0 +1,42 @@
+document.querySelector('form').addEventListener('submit', addMovie);
+const message = document.querySelector('#message');
+
+function addMovie(event) {
+event.preventDefault();
+
+const inputField = document.querySelector('input');
+
+const movie = document.createElement('li');
+
+const movieTitle = document.createElement('span');
+movieTitle.textContent = inputField.value;
+movieTitle.addEventListener('click', crossOffMovie);
+movie.appendChild(movieTitle);
+
+const deleteBtn = document.createElement('button');
+deleteBtn.textContent = 'X';
+deleteBtn.addEventListener('click', deleteMovie);
+movie.appendChild(deleteBtn);
+
+const list = document.querySelector('ul');
+list.appendChild(movie);
+
+inputField.value = '';
+}
+
+function deleteMovie(event) {
+    event.target.parentNode.remove()
+    message.textContent = 'movie was deleted from list'
+}
+
+function crossOffMovie(event) {
+    event.target.classList.toggle('checked')
+    if(event.target.classList.contains('checked') === true) {
+        message.textContent = 'you have already seen this movie'
+    } else {
+        message.textContent = 'you have added this movie back'
+    }
+}
+
+
+
